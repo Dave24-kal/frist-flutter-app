77ace894c59edb68fc5318aef8a15deb5467cce3
Dave24-kal <kaldave548@gmail.com>
2025-12-15 18:14:35 +0300

    Remove commented-out Flutter app code
    Removed commented-out code for the main Flutter app.

--- lib/main.dart
+++ lib/main.dart
@@ -53,46 +53,4 @@ class _CounterAppState extends State<counterApp> {
   }
 }
 
-/**import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(home: CounterPage());
-  }
-}
-
-class CounterPage extends StatefulWidget {
-  const CounterPage({super.key});
-
-  @override
-  State<CounterPage> createState() => _CounterPageState();
-}
-
-class _CounterPageState extends State<CounterPage> {
-  int counter = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Counter App')),
-      body: Center(
-        child: Text('Counter: $counter', style: const TextStyle(fontSize: 30)),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            counter++;
-          });
-        },
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}**/

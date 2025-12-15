74009dd231dcd527b9f0b8c904d96e302d19afa6
Dawit Kalbesa <kaldave548@gmail.com>
2025-12-15 18:02:12 +0300

    add minus and referesh button

--- lib/main.dart
+++ lib/main.dart
@@ -20,14 +20,79 @@ class _CounterAppState extends State<counterApp> {
         child: Text('counter: $counter', style: TextStyle(fontSize: 30)),
       ),
 
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                counter--;
+              });
+            },
+            child: Icon(Icons.remove),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                counter++;
+              });
+            },
+            child: Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                counter = 0;
+              });
+            },
+            child: Icon(Icons.refresh),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/**import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: CounterPage());
+  }
+}
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Counter App')),
+      body: Center(
+        child: Text('Counter: $counter', style: const TextStyle(fontSize: 30)),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           setState(() {
             counter++;
           });
         },
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
     );
   }
-}
+}**/
